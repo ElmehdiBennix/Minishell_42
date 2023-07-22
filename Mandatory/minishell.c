6dbecf0fad09c4b56f6902a9e8ed058a73e3b165
El Mehdi Bennix <ebennix@e3r2p8.1337.ma>
2023-07-22 04:15:51 +0100

    MakefileAutoPush

--- Mandatory/minishell.c
+++ Mandatory/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 21:05:35 by ebennix           #+#    #+#             */
-/*   Updated: 2023/07/22 03:17:44 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/07/22 03:24:51 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void minishell()
         content = readline("le minishit : "); // should display corrent dir and exit msgs zith colors
         ft_fprintf(1,"%s\n",content);
     }
-}
+}=
 
 int main()
 {
