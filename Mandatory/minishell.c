469d50e045d8916e5da2c55467088468ec118984
ElmehdiBennix <bennixmehdi@gmail.com>
2023-07-22 03:16:56 +0100

    MakefileAutoPush

--- Mandatory/minishell.c
+++ Mandatory/minishell.c
@@ -6,14 +6,27 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 21:05:35 by ebennix           #+#    #+#             */
-/*   Updated: 2023/07/22 01:23:14 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/07/22 03:16:49 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/minishell.h"
+#include <readline/readline.h>
 
-int main (int ac, char **av)
+void minishell()
 {
-    
+    char *content;
+
+    while(1)
+    {
+        content = readline("le minishit : ");
+        ft_fprintf(1,"%s\n",content);
+    }
+}
+
+int main()
+{
+    extern char **environ;
+    minishell();
 
 }
