93e70ac1c79ab97549736b75c1c7cb08d575962a
Omar Taraki <otaraki@e3r2p17.1337.ma>
2023-07-24 01:09:08 +0100

    MakefileAutoPush

--- Mandatory/minishell.c
+++ Mandatory/minishell.c
@@ -6,13 +6,13 @@
 /*   By: otaraki <otaraki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 23:17:11 by otaraki           #+#    #+#             */
-/*   Updated: 2023/07/23 04:14:23 by otaraki          ###   ########.fr       */
+/*   Updated: 2023/07/24 00:06:09 by otaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/minishell.h"
 
-
+// "nobody told you that the journey gonna be that easy" --> stop thiniking -> start acting ??  
 void get_fstr()
 {
     char *content;
@@ -22,7 +22,8 @@ void get_fstr()
         content= readline("Mininshell :");
         // printf("%s\n", content);
         content = ft_strtrim(content, " ");
-        tokeni(content);
+        valid_cmd(content);
+        // tokeni(content);
     }
 }
 
