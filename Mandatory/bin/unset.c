b36850092e0857baaa649dbbd4bb426d117abc0f
Omar Taraki <otaraki@e3r4p5.1337.ma>
2023-08-01 15:08:12 +0100

    just sumilated the mini mimi lol

--- Mandatory/bin/unset.c
+++ Mandatory/bin/unset.c
@@ -6,7 +6,7 @@
 /*   By: otaraki <otaraki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 09:53:35 by otaraki           #+#    #+#             */
-/*   Updated: 2023/08/01 14:54:49 by otaraki          ###   ########.fr       */
+/*   Updated: 2023/08/01 15:07:10 by otaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void ft_unset(char **arg, t_env *env)
 {
 	int i;
 
-	i = 2;
+	i = 1;
 	while(arg[i])
 	{
 		ft_remove_node(&env, arg[i], ft_strcmp);
