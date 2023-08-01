412d6ae36e978d01f216ce14e3b2c5f9094d36b6
Omar Taraki <otaraki@e3r4p5.1337.ma>
2023-08-01 14:46:40 +0100

    MakefileAutoPush

--- Mandatory/bin/unset.c
+++ Mandatory/bin/unset.c
@@ -6,7 +6,7 @@
 /*   By: otaraki <otaraki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 09:53:35 by otaraki           #+#    #+#             */
-/*   Updated: 2023/08/01 13:11:05 by otaraki          ###   ########.fr       */
+/*   Updated: 2023/08/01 14:44:27 by otaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 
 void ft_unset(char *option, char *val, t_env *env)
 {
-	if (option)
-	{
-		if (!ft_strcmp(option, "-v"))
-	}
+	(void)option;
 	ft_remove_node(&env, val, ft_strcmp);
 	while(env)
     {
