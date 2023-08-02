732957df3d500b163fec9ca04216829e25d9a834
Omar Taraki <otaraki@e3r11p9.1337.ma>
2023-08-02 12:26:59 +0100

    still working on built-in cd

--- Mandatory/inc/minishell.h
+++ Mandatory/inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: otaraki <otaraki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 01:12:28 by ebennix           #+#    #+#             */
-/*   Updated: 2023/08/01 14:55:07 by otaraki          ###   ########.fr       */
+/*   Updated: 2023/08/02 11:46:38 by otaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,9 +51,13 @@ t_env	*ft_lstnew_env(char *key, char *value);
 void	ft_lstadd_back_env(t_env **lst, t_env *new);
 int		ft_strcmp(char *s1, char *s2);
 void 	ft_remove_node(t_env **begin_list, void *data_ref, int (*cmp)());
-void 	ft_unset(char **arg, t_env *env);
+char	*key_by_value(t_env *env, char *value);
+char	*value_by_key(t_env *env, char *key);
+//built-in functions:
 void	ft_bultin(char **arg, t_env *env);
+void 	ft_unset(char **arg, t_env *env);
 void	ft_env(t_env *env);
+void	me_cd(char **av, t_env *env);
 
 // void tokeni(char *f_line);
 
--- Mandatory/utils/get_key.c
+++ Mandatory/utils/get_key.c
@@ -1,23 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bultins.c                                       :+:      :+:    :+:   */
+/*   get_key.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: otaraki <otaraki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/01 09:50:58 by otaraki           #+#    #+#             */
-/*   Updated: 2023/08/02 12:10:09 by otaraki          ###   ########.fr       */
+/*   Created: 2023/08/02 11:39:43 by otaraki           #+#    #+#             */
+/*   Updated: 2023/08/02 11:46:41 by otaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-void	ft_bultin(char **arg, t_env *env)
+char	*value_by_key(t_env *env, char *key)
 {
-	if (ft_strcmp(arg[0], "env") == 0)
-		ft_env(env);
-	else if (ft_strcmp(arg[0], "unset") == 0)
-		ft_unset(arg, env);
-	else if (ft_strcmp(arg[0], "cd") == 0)
-		me_cd(arg, env);
+	t_env *tmp;
+
+	tmp = env;
+	while (tmp)
+	{
+		if (ft_strcmp(tmp->key, key) == 0)
+			return(tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
+
+char	*key_by_value(t_env *env, char *value)
+{
+	t_env *tmp;
+
+	tmp = env;
+	while (tmp)
+	{
+		if (ft_strcmp(tmp->value, value) == 0)
+			return (tmp->key);
+		tmp = tmp->next;
+	}
+	return (NULL);
 }
