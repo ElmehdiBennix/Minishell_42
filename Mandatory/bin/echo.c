9dcea66202076cd492be745ec9b12fd3dd634b27
Omar Taraki <otaraki@e3r7p11.1337.ma>
2023-08-03 12:03:09 +0100

    just finshed the echo cmd, check leaks later ;)

--- Mandatory/bin/echo.c
+++ Mandatory/bin/echo.c
@@ -6,7 +6,7 @@
 /*   By: otaraki <otaraki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 09:53:03 by otaraki           #+#    #+#             */
-/*   Updated: 2023/08/03 10:53:40 by otaraki          ###   ########.fr       */
+/*   Updated: 2023/08/03 12:01:18 by otaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,23 @@
 static int	check_option(char **av)
 {
 	int	i;
+	int j;
 	int	flg;
 	
 	flg = 0;
 	i = 1;
 	while (av[i])
 	{
-		if (!ft_strcmp(av[i], "-n"))
-			flg += 1;
+		if (!ft_strncmp(av[i], "-n", 2))
+		{
+			j = 1;
+			while(av[i][j] && av[i][j] == 'n')
+				j++;
+			if (!av[i][j])
+				flg += 1;
+			else
+				return (flg);
+		}
 		i++;
 	}
 	return (flg);
@@ -33,16 +42,21 @@ void	my_echo(char **av, t_env *env)
 {
 	int	i;
 	int	option;
+	int flag;
 
 	(void)env;
 	i = 1;
 	option = check_option(av);
 	if (option != 0)
 		i += option;
+	flag = 1;
 	while (av[i])
 	{
+		if (!flag)
+			write(1, " ", 1);
 		ft_putstr_fd(av[i], 1);// file descriptor not set yet
-		write(1, " ", 1);// should handle space befor args
+		flag = 0;
+		// write(1, " ", 1);// should handle space befor args
 		i++;
 	}
 	if (option == 0)
