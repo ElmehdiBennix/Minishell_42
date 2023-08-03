8108ff7689d097479b46b6349811061efcb66be8
Omar Taraki <otaraki@e3r7p15.1337.ma>
2023-08-03 14:15:58 +0100

    still working on exit

--- Mandatory/bin/exit.c
+++ Mandatory/bin/exit.c
@@ -6,8 +6,24 @@
 /*   By: otaraki <otaraki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 09:53:18 by otaraki           #+#    #+#             */
-/*   Updated: 2023/08/01 09:53:19 by otaraki          ###   ########.fr       */
+/*   Updated: 2023/08/03 14:10:24 by otaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#include "../inc/minishell.h"
+
+void	mini_exit(t_env *env, int i)
+{
+	t_env	*tmp;
+
+	while (tmp)
+	{
+		if (tmp->key)
+			free(tmp->key);
+		if (tmp->value)
+			free(tmp->value);
+		tmp = tmp->next;
+	}
+	exit(0);
+}
