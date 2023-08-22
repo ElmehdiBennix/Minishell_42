fd8cb53ab43ef7f798cfcffab9b4211d5fd779fe
ElmehdiBennix <bennixmehdi@gmail.com>
2023-08-22 23:31:08 +0100

    MakefileAutoPush

--- Mandatory/inc/minishell.h
+++ Mandatory/inc/minishell.h
@@ -6,50 +6,50 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 01:12:28 by ebennix           #+#    #+#             */
-/*   Updated: 2023/08/22 22:55:56 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/08/22 23:02:11 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <readline/readline.h>
-#include <readline/history.h>
-#include "../../lib/inc/utils.h"
+# include "../../lib/inc/utils.h"
+# include <readline/history.h>
+# include <readline/readline.h>
 
 typedef enum s_type
 {
-    WORD = 1,
+	WORD = 1,
 	SINGLE_QUOT = 2,
 	DOUBLE_QUOT = 3,
 	PIPE = 4,
 	GREAT = 5,
 	LESS = 6,
-	APPEND = 7, // >>
+	APPEND = 7,   // >>
 	HERE_DOC = 8, // <<
 
-}	t_type;
+}					t_type;
 
 typedef struct s_token
 {
-    int             id;
-    char            *content;
-    bool            space_after;
-    t_type          type;
-    struct s_token  *forward;
-    struct s_token  *backward;
-} t_token;
+	int				id;
+	char			*content;
+	bool			space_after;
+	t_type			type;
+	struct s_token	*forward;
+	struct s_token	*backward;
+}					t_token;
 
 typedef struct s_mini_data
 {
-    int             err_no;
-    char            **env_var;
-	char	        **path_var;
-    char            *PWD;
-    char            *OLDPWD;
-    struct s_token  *tokens;
+	int				err_no;
+	char			**env_var;
+	char			**path_var;
+	char			*PWD;
+	char			*OLDPWD;
+	struct s_token	*tokens;
 
-}   t_mini_data;
+}					t_mini_data;
 
 // typedef      //pipe split commands
 // {
@@ -66,20 +66,18 @@ typedef struct s_mini_data
 // back
 // each node has a **pointer next means pipe
 
-
 # define TRUE 1
 # define FALSE 0
 
-void    get_type(t_token *token , int moves);
+void				get_type(t_token *token, int moves);
 
-int ft_iseparateur(char c);
-int ft_iswhite_space(char c);
-void    ft_lstdoubly(t_token **head , t_token *node);
+int					ft_iseparateur(char c);
+int					ft_iswhite_space(char c);
+void				ft_lstdoubly(t_token **head, t_token *node);
 
-t_token *get_tokens(char *prompt);
-void tokenizer(t_token *tokens);
-bool shell_history(t_mini_data *var, char *prompt);
-void    get_type(t_token *token , int moves);
-
+t_token				*get_tokens(char *prompt);
+bool				tokenizer(t_token *tokens);
+bool				shell_history(t_mini_data *var, char *prompt);
+void				get_type(t_token *token, int moves);
 
 #endif
--- Mandatory/mini_parse/get_tokens.c
+++ Mandatory/mini_parse/get_tokens.c
@@ -6,118 +6,119 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 23:36:05 by otaraki           #+#    #+#             */
-/*   Updated: 2023/08/22 22:56:57 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/08/22 23:06:19 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-static t_token   *char_handler(char *prompt, int *i)
+static t_token	*char_handler(char *prompt, int *i)
 {
-    int moves;
-    int backward;
-    t_token *token = NULL;
-    
-    moves = 0;
-    token = (t_token *)ft_calloc(sizeof(t_token),1);
-    while(prompt[*i] && ft_iswhite_space(prompt[*i]) == 0 && ft_iseparateur(prompt[*i]) == 0)
-    {
-        (*i)++;
-        moves++;
-    }
-    backward = (*i) - moves;
-    token->content = ft_calloc(moves + 1, sizeof(char));
-    ft_strlcpy(token->content,&prompt[backward],moves + 1);
-    token->type = WORD;
-    if (prompt[*i] == ' ')
-        token->space_after = 1;
-    else
-        token->space_after = 0;
-    return (token); 
+	int		moves;
+	int		backward;
+	t_token	*token;
+
+	moves = 0;
+	token = (t_token *)ft_calloc(sizeof(t_token), 1);
+	while (prompt[*i] && ft_iswhite_space(prompt[*i]) == 0
+		&& ft_iseparateur(prompt[*i]) == 0)
+	{
+		(*i)++;
+		moves++;
+	}
+	backward = (*i) - moves;
+	token->content = ft_calloc(moves + 1, sizeof(char));
+	ft_strlcpy(token->content, &prompt[backward], moves + 1);
+	token->type = WORD;
+	if (prompt[*i] == ' ')
+		token->space_after = 1;
+	else
+		token->space_after = 0;
+	return (token);
 }
 
-static t_token   *QUOT_handler(char *prompt, int *i , char QUOT_type)
+static t_token	*QUOT_handler(char *prompt, int *i, char QUOT_type)
 {
-    int moves;
-    int backward;
-    t_token *token;
-    
-    moves = 0;
-    token = (t_token *)ft_calloc(sizeof(t_token),1);
-    (*i)++;
-    while (prompt[*i] != QUOT_type)
-    {
-        (*i)++;
-        moves++;
-    }
-    (*i)++;
-    backward = (*i) - moves - 2;
-    token->content = ft_calloc(moves + 3, sizeof(char));
-    ft_strlcpy(token->content,&prompt[backward],moves + 3);
-    if (prompt[*i] == ' ')
-        token->space_after = 1;
-    else
-        token->space_after = 0;
-    if (QUOT_type == 39)
-        token->type = SINGLE_QUOT;
-    else
-        token->type = DOUBLE_QUOT;
-    return (token);
+	int		moves;
+	int		backward;
+	t_token	*token;
+
+	moves = 0;
+	token = (t_token *)ft_calloc(sizeof(t_token), 1); // protect
+	(*i)++;
+	while (prompt[*i] != QUOT_type)
+	{
+		(*i)++;
+		moves++;
+	}
+	(*i)++;
+	backward = (*i) - moves - 2;
+	token->content = ft_calloc(moves + 3, sizeof(char)); // protect
+	ft_strlcpy(token->content, &prompt[backward], moves + 3);
+	if (prompt[*i] == ' ')
+		token->space_after = 1;
+	else
+		token->space_after = 0;
+	if (QUOT_type == 39)
+		token->type = SINGLE_QUOT;
+	else
+		token->type = DOUBLE_QUOT;
+	return (token);
 }
 
-static t_token   *separateur_handler(char *prompt, int *i)
+static t_token	*separateur_handler(char *prompt, int *i)
 {
-    int moves;
-    int backward;
-    t_token *token;
-    
-    moves = 0;
-    token = (t_token *)ft_calloc(sizeof(t_token),1);
-    if (prompt[*i] == '|')
-    {
-        (*i)++;
-        moves++;
-    }
-    else if (prompt[*i] == '>' || prompt[*i] == '<')
-    {
-        (*i)++;
-        moves++;
-        if (prompt[*i] && (prompt[*i] == '>' || prompt[*i] == '<'))
-        {
-            (*i)++;
-            moves++;
-        }
-    }
-    backward = (*i) - moves;
-    token->content = ft_calloc(moves + 1, sizeof(char));
-    ft_strlcpy(token->content,&prompt[backward],moves + 1);
-    get_type(token ,moves);
-    if (prompt[*i] == ' ')
-        token->space_after = 1;
-    else
-        token->space_after = 0;
-    return (token);
+	int		moves;
+	int		backward;
+	t_token	*token;
+
+	moves = 0;
+	token = (t_token *)ft_calloc(sizeof(t_token), 1);
+	if (prompt[*i] == '|')
+	{
+		(*i)++;
+		moves++;
+	}
+	else if (prompt[*i] == '>' || prompt[*i] == '<')
+	{
+		(*i)++;
+		moves++;
+		if (prompt[*i] && (prompt[*i] == '>' || prompt[*i] == '<'))
+		{
+			(*i)++;
+			moves++;
+		}
+	}
+	backward = (*i) - moves;
+	token->content = ft_calloc(moves + 1, sizeof(char));
+	ft_strlcpy(token->content, &prompt[backward], moves + 1);
+	get_type(token, moves);
+	if (prompt[*i] == ' ')
+		token->space_after = 1;
+	else
+		token->space_after = 0;
+	return (token);
 }
 
-
-t_token *get_tokens(char *prompt)
+t_token	*get_tokens(char *prompt)
 {
-    int     i;
-    t_token *tokens = NULL;
-    
-    i = 0;
-    while (prompt[i])
-    {
-        if (ft_iswhite_space(prompt[i]) == 1)
-            i++;
-        else if (prompt[i] == '|' || prompt[i] == '<' || prompt[i] == '>')
-            ft_lstdoubly(&tokens, separateur_handler(prompt, &i));
-        else if (prompt[i] == 34 || prompt[i] == 39)
-            ft_lstdoubly(&tokens, QUOT_handler(prompt, &i , prompt[i]));
-        else
-            ft_lstdoubly(&tokens, char_handler(prompt, &i));
-    }
-    return(tokens);
+	int		i;
+	t_token	*tokens;
+
+	tokens = NULL;
+	i = 0;
+	while (prompt[i])
+	{
+		if (ft_iswhite_space(prompt[i]) == 1)
+			i++;
+		else if (prompt[i] == '|' || prompt[i] == '<' || prompt[i] == '>')
+			ft_lstdoubly(&tokens, separateur_handler(prompt, &i));
+		else if (prompt[i] == 34 || prompt[i] == 39)
+			ft_lstdoubly(&tokens, QUOT_handler(prompt, &i, prompt[i]));
+		else
+			ft_lstdoubly(&tokens, char_handler(prompt, &i));
+	}
+	return (tokens);
 }
 // fix data ttypes for better code too and leaks of course and manage his shit
 // leaks left and whole lots of code managements
