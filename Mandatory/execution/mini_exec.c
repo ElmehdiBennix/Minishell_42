8a9a591662e2323568760ceaa4b3b8eeae603ecf
ElmehdiBennix <bennixmehdi@gmail.com>
2023-08-23 00:10:11 +0100

    MakefileAutoPush

--- Mandatory/execution/mini_exec.c
+++ Mandatory/execution/mini_exec.c
@@ -1,38 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*   mini_exec.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/21 21:06:46 by ebennix           #+#    #+#             */
-/*   Updated: 2023/08/23 00:07:28 by ebennix          ###   ########.fr       */
+/*   Created: 2023/08/23 00:09:08 by ebennix           #+#    #+#             */
+/*   Updated: 2023/08/23 00:09:10 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/minishell.h"
-
-t_env *ft_lstlast_env(t_env *node)
-{
-	if (!node)
-		return NULL;
-	if (node->next == NULL)
-		return node;
-	while (node->next)
-	{
-		node = node->next;
-	}
-	return node;
-}
-void	ft_lstadd_back_env(t_env **lst, t_env *new) // useless already in libft
-{
-	t_env	*arrow;
-
-	if (*lst == NULL)
-		*lst = new;
-	else
-	{
-		arrow = ft_lstlast_env(*lst);
-		arrow -> next = new;
-	}
-}
