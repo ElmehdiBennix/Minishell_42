6588f4deb9100272866205399a39f43b38af93e2
El mehdi bennix <bennixmehdi@gmail.com>
2023-08-30 01:07:25 +0100

    MakefileAutoPush

--- Mandatory/grouper/group.c
+++ Mandatory/grouper/group.c
@@ -6,7 +6,30 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 00:04:51 by ebennix           #+#    #+#             */
-/*   Updated: 2023/08/30 00:04:57 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/08/30 00:33:03 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/minishell.h"
+
+static  int ft_lstsizee(t_token *lst)
+{
+	int	i;
+
+	i = 0;
+	while (lst)
+	{
+        if (lst->type != PIPE)
+		    i++;
+		lst = lst -> forward;
+	}
+	return (i);
+}
+
+bool group_args(t_mini_data *var)
+{
+    int size = ft_lstsizee(var->tokens);
+    printf("%d\n",size);
+    
+    
+}
