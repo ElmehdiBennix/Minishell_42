d090c6a39bce0bb30790b3abb3ef6a12dcb11e5e
ElmehdiBennix <bennixmehdi@gmail.com>
2023-09-24 03:45:08 +0100

    MakefileAutoPush

--- Mandatory/minishell.c
+++ Mandatory/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 01:39:52 by ebennix           #+#    #+#             */
-/*   Updated: 2023/09/24 03:42:52 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/09/24 03:45:05 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -114,27 +114,27 @@ static bool	parse_loop(t_mini_data *var, char *prompt)
 	if (linker(var) == TRUE) // leaks left
         return(cmd_free(var->exec_data,1),tok_free(var->tokens,1),var->err_no = 8 ,1);
     tok_free(var->tokens,1);
-    // t_command_table *test = var->exec_data;
-    // int i = 0;
-    // while(test)
-    // {
-    //     t_redirection *test2 = test->redirections;
-    //     while(test->cmds_array[i])
-    //     {
-    //         printf("command[%d] == %s\n",i,test->cmds_array[i]);
-    //         i++;
-    //     }
-	//     printf("command[%d] == %s\n",i,test->cmds_array[i]);
-    //     i = 0;
-    //     while(test2)
-    //     {
-    //         printf("type = %d | file = %s\n",test2->r_type,test2->file_name);
-    //         test2 = test2->next;
-    //     }
-	// 	printf("file = %p\n",test2);
-    //     test = test->forward;
-    //     printf("end\n");
-    // }
+    t_command_table *test = var->exec_data;
+    int i = 0;
+    while(test)
+    {
+        t_redirection *test2 = test->redirections;
+        while(test->cmds_array[i])
+        {
+            printf("command[%d] == %s\n",i,test->cmds_array[i]);
+            i++;
+        }
+	    printf("command[%d] == %s\n",i,test->cmds_array[i]);
+        i = 0;
+        while(test2)
+        {
+            printf("type = %d | file = %s\n",test2->r_type,test2->file_name);
+            test2 = test2->next;
+        }
+		printf("file = %p\n",test2);
+        test = test->forward;
+        printf("end\n");
+    }
     return(0);
 }
 
