ae87798ce656b26acf8a0cc8bdc1c745872c90f3
El Mehdi Bennix <ebennix@e3r10p1.1337.ma>
2023-09-26 23:00:18 +0100

    MakefileAutoPush

--- Mandatory/execution/pipe_handler.c
+++ Mandatory/execution/pipe_handler.c
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 20:57:12 by otaraki           #+#    #+#             */
-/*   Updated: 2023/09/26 07:03:31 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/09/26 22:46:34 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,11 @@ void	multi_cmd(t_command_table *exec_data, t_env **env)
 			ft_bultin(arrow, env); // catch errors
 		pipe(fd);
 		pid = fork();
+		if (pid == -1)
+		{
+			printf("error\n");
+			exit(1);
+		}
 		if (!pid)
 		{
 			if (arrow->fdin != 0)
