2b304e9f1d8a42cb51b6b4e3e8e62541261ec4e5
Omar Taraki <otaraki@e3r2p9.1337.ma>
2023-09-28 02:45:43 +0100

    MakefileAutoPush

--- Mandatory/utils/updates.c
+++ Mandatory/utils/updates.c
@@ -6,7 +6,7 @@
 /*   By: otaraki <otaraki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 15:05:44 by otaraki           #+#    #+#             */
-/*   Updated: 2023/09/28 02:37:34 by otaraki          ###   ########.fr       */
+/*   Updated: 2023/09/28 02:41:19 by otaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,4 +38,26 @@ t_env	*update_env(t_env **env, char *value, char *key)
 		ft_lstadd_back_env(&tmp, ft_lstnew_env(ft_strdup(key), \
 			ft_strdup(value)));
 	return (tmp);
+}
+
+void	unlink_opened_files(void)
+{
+	int		i;
+	char	*str; 
+	char	*s;
+
+	i = 0;
+	str = ft_itoa(i);
+	s = ft_strjoin("/tmp/here_doc", str);
+	free(str);
+	while (access(s, F_OK) == 0)
+	{
+		str = NULL;
+		unlink(s);
+		free(s);
+		i++;
+		str = ft_itoa(i);
+		s = ft_strjoin("/tmp/here_doc", str);
+		free(str);
+	}
 }
