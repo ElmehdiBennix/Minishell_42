fdd5bec2fd3f17634994896fc633e83ff07f42ac
ElmehdiBennix <bennixmehdi@gmail.com>
2023-10-05 07:09:09 +0100

    MakefileAutoPush

--- Mandatory/parse/parser.c
+++ Mandatory/parse/parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bennix <bennix@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 03:18:41 by ebennix           #+#    #+#             */
-/*   Updated: 2023/10/05 01:10:56 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/10/05 07:02:14 by bennix           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,27 @@
 bool	forbidden_tokens(t_token *token)
 {
 	if ((token->type == PIPE && token->forward->type == PIPE)
-	|| (token->type == GREAT && token->forward->type == GREAT)
-	|| (token->type == GREAT && token->forward->type == LESS)
-	|| (token->type == GREAT && token->forward->type == APPEND)
-	|| (token->type == GREAT && token->forward->type == HERE_DOC)
-	|| (token->type == LESS && token->forward->type == GREAT)
-	|| (token->type == LESS && token->forward->type == LESS)
-	|| (token->type == LESS && token->forward->type == APPEND)
-	|| (token->type == LESS && token->forward->type == HERE_DOC)
-	|| (token->type == LESS && token->forward->type == PIPE)
-	|| (token->type == HERE_DOC && token->forward->type == GREAT)
-	|| (token->type == HERE_DOC && token->forward->type == LESS)
-	|| (token->type == HERE_DOC && token->forward->type == APPEND)
-	|| (token->type == HERE_DOC && token->forward->type == HERE_DOC)
-	|| (token->type == HERE_DOC && token->forward->type == PIPE)
-	|| (token->type == APPEND && token->forward->type == GREAT)
-	|| (token->type == APPEND && token->forward->type == LESS)
-	|| (token->type == APPEND && token->forward->type == APPEND)
-	|| (token->type == APPEND && token->forward->type == HERE_DOC)
-	|| (token->type == APPEND && token->forward->type == PIPE)
-	|| (token->type == GREAT && token->space_after == TRUE
-		&& token->forward->type == PIPE))
+		|| (token->type == GREAT && token->forward->type == GREAT)
+		|| (token->type == GREAT && token->forward->type == LESS)
+		|| (token->type == GREAT && token->forward->type == APPEND)
+		|| (token->type == GREAT && token->forward->type == HERE_DOC)
+		|| (token->type == LESS && token->forward->type == GREAT)
+		|| (token->type == LESS && token->forward->type == LESS)
+		|| (token->type == LESS && token->forward->type == APPEND)
+		|| (token->type == LESS && token->forward->type == HERE_DOC)
+		|| (token->type == LESS && token->forward->type == PIPE)
+		|| (token->type == HERE_DOC && token->forward->type == GREAT)
+		|| (token->type == HERE_DOC && token->forward->type == LESS)
+		|| (token->type == HERE_DOC && token->forward->type == APPEND)
+		|| (token->type == HERE_DOC && token->forward->type == HERE_DOC)
+		|| (token->type == HERE_DOC && token->forward->type == PIPE)
+		|| (token->type == APPEND && token->forward->type == GREAT)
+		|| (token->type == APPEND && token->forward->type == LESS)
+		|| (token->type == APPEND && token->forward->type == APPEND)
+		|| (token->type == APPEND && token->forward->type == HERE_DOC)
+		|| (token->type == APPEND && token->forward->type == PIPE)
+		|| (token->type == GREAT && token->space_after == TRUE
+			&& token->forward->type == PIPE))
 		return (1);
 	return (0);
 }
@@ -46,9 +46,9 @@ bool	tokens_checker(t_token *token, int *nodes)
 
 	tmp = NULL;
 	if (forbidden_tokens(token) == TRUE)
-		return(ft_fprintf(2, "le minishell: syntax error near unexpected token `%s'\n", token->forward->content), 1);
+		return (ft_fprintf(2, SYNX_6, token->forward->content), 1);
 	else if ((token->type == GREAT && token->space_after == FALSE
-				&& token->forward->type == PIPE))
+			&& token->forward->type == PIPE))
 	{
 		tmp = token->forward;
 		token->forward->forward->backward = token;
@@ -78,7 +78,7 @@ bool	parser(t_mini_data *var)
 		else if (arrow->type >= WORD && arrow->type <= DOUBLE_QUOT)
 			token_number = 0;
 		if (token_number == 3 || arrow->type == 10)
-			return (ft_fprintf(2, "le minishell: syntax error near unexpected token `%s'\n", arrow->content), 1);
+			return (ft_fprintf(2, SYNX_6, arrow->content), 1);
 		arrow = arrow->forward;
 	}
 	return (0);
